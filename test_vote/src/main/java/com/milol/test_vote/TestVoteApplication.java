869c6281dfdc4c73b00ff71a038285258645ff25
clooney lysandre <bayegui.milolpro@gmail.com>
2022-08-11 13:26:19 +0100

    this is my real first commit in which i'm sending some files

---
+++ test_vote/src/main/java/com/milol/test_vote/TestVoteApplication.java
@@ -0,0 +1,13 @@
+package com.milol.test_vote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestVoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestVoteApplication.class, args);
+	}
+
+}
---
+++ test_vote/src/test/java/com/milol/test_vote/TestVoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milol.test_vote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestVoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
